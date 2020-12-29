ed3eb51928e55e687498225ff0f3ebc27ad95c88
doctarhyf <drrhyf@gmail.com>
2020-12-29 21:17:23 +0200

    added bootstrap vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img width="100px" alt="Vue logo" src="./assets/flame.png">
+    <!-- <HelloWorld msg="🤣Cool Welcome to Your Vue.js App"/> -->
+    <LshiSec name="my name new components 😁😝"></LshiSec>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LshiSec from './components/LshiSec.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LshiSec
   }
 }
 </script>
