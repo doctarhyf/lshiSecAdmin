43b2655bc9493442d4b07635979f43a94b42cb29
doctarhyf <drrhyf@gmail.com>
2020-12-29 22:24:51 +0200

    changes

--- src/components/LshiSec.vue
+++ src/components/LshiSec.vue
@@ -2,32 +2,23 @@
   <div >
   
     
-<ul class="uk-grid-small uk-child-width-1-2 uk-child-width-1-4@s" uk-sortable="handle: .uk-card" uk-grid>
-    <li>
-        <div class="uk-card uk-card-default uk-card-body uk-text-center">Item 1</div>
-    </li>
-    <li>
-        <div class="uk-card uk-card-default uk-card-body uk-text-center">Item 2</div>
-    </li>
-    <li>
-        <div class="uk-card uk-card-default uk-card-body uk-text-center">Item 3</div>
-    </li>
-    <li>
-        <div class="uk-card uk-card-default uk-card-body uk-text-center">Item 4</div>
-    </li>
-    <li>
-        <div class="uk-card uk-card-default uk-card-body uk-text-center">Item 5</div>
-    </li>
-    <li>
-        <div class="uk-card uk-card-default uk-card-body uk-text-center">Item 6</div>
-    </li>
-    <li>
-        <div class="uk-card uk-card-default uk-card-body uk-text-center">Item 7</div>
-    </li>
-    <li>
-        <div class="uk-card uk-card-default uk-card-body uk-text-center">Item 8</div>
-    </li>
-</ul>
+<!-- This is a button toggling the modal -->
+<button class="uk-button uk-button-default uk-margin-small-right" type="button" uk-toggle="target: #modal-example">Open</button>
+
+<!-- This is an anchor toggling the modal -->
+<a href="#modal-example" uk-toggle>Open</a>
+
+<!-- This is the modal -->
+<div id="modal-example" uk-modal>
+    <div class="uk-modal-dialog uk-modal-body">
+        <h2 class="uk-modal-title">Headline</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p class="uk-text-right">
+            <button class="uk-button uk-button-default uk-modal-close" type="button">Cancel</button>
+            <button class="uk-button uk-button-primary" type="button">Save</button>
+        </p>
+    </div>
+</div>
   
   </div>
 
